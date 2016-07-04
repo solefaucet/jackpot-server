3a1a142751700d0d2368609896c5c7e97f8a2eeb
on99 <on99@users.noreply.github.com>
2016-07-04 22:51:17 +0800

    test(mysql): fix broken test

--- services/storage/mysql/transaction_test.go
+++ services/storage/mysql/transaction_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/jmoiron/sqlx"
 	. "github.com/smartystreets/goconvey/convey"
 	"github.com/solefaucet/jackpot-server/models"
 )
@@ -13,21 +14,23 @@ func TestSaveTransactions(t *testing.T) {
 		s := prepareDatabaseForTesting()
 
 		Convey("When save duplicated transactions", func() {
-			err := s.SaveTransactions([]models.Transaction{
-				{
-					Address:        "addr",
-					Amount:         10,
-					TransactionID:  "id",
-					Hash:           "hash",
-					BlockCreatedAt: time.Now(),
-				},
-				{
-					Address:        "addr",
-					Amount:         10,
-					TransactionID:  "id",
-					Hash:           "hash",
-					BlockCreatedAt: time.Now(),
-				},
+			err := s.withTx(func(tx *sqlx.Tx) error {
+				return saveTransactions(tx, []models.Transaction{
+					{
+						Address:        "addr",
+						Amount:         10,
+						TransactionID:  "id",
+						Hash:           "hash",
+						BlockCreatedAt: time.Now(),
+					},
+					{
+						Address:        "addr",
+						Amount:         10,
+						TransactionID:  "id",
+						Hash:           "hash",
+						BlockCreatedAt: time.Now(),
+					},
+				})
 			})
 
 			Convey("Error should not be nil", func() {
@@ -40,70 +43,21 @@ func TestSaveTransactions(t *testing.T) {
 		s := prepareDatabaseForTesting()
 
 		Convey("When save transactions", func() {
-			err := s.SaveTransactions([]models.Transaction{
-				{
-					Address:        "addr",
-					Amount:         10,
-					TransactionID:  "id",
-					Hash:           "hash",
-					BlockCreatedAt: time.Now(),
-				},
+			err := s.withTx(func(tx *sqlx.Tx) error {
+				return saveTransactions(tx, []models.Transaction{
+					{
+						Address:        "addr",
+						Amount:         10,
+						TransactionID:  "id",
+						Hash:           "hash",
+						BlockCreatedAt: time.Now(),
+					},
+				})
 			})
 
 			Convey("Error should be nil", func() {
 				So(err, ShouldBeNil)
 			})
 		})
-	})
-}
-
-func TestGetLatestTransactionBlockHash(t *testing.T) {
-	Convey("Given empty mysql storage", t, func() {
-		s := prepareDatabaseForTesting()
-
-		Convey("When get latest block", func() {
-			hash, err := s.GetLatestTransactionBlockHash()
-
-			Convey("Error should be nil", func() {
-				So(err, ShouldBeNil)
-			})
-
-			Convey("Hash should be empty", func() {
-				So(hash, ShouldBeEmpty)
-			})
-		})
-	})
-
-	Convey("Given mysql storage with transaction data", t, func() {
-		s := prepareDatabaseForTesting()
-		s.SaveTransactions([]models.Transaction{
-			{
-				Address:        "addr",
-				Amount:         10,
-				TransactionID:  "id1",
-				Hash:           "hash1",
-				BlockCreatedAt: time.Now(),
-			},
-			{
-				Address:        "addr",
-				Amount:         10,
-				TransactionID:  "id2",
-				Hash:           "hash2",
-				BlockCreatedAt: time.Now().Add(time.Hour),
-			},
-		})
-
-		Convey("When get latest transaction block hash", func() {
-			hash, _ := s.GetLatestTransactionBlockHash()
-
-			Convey("Hash should be hash2", func() {
-				So(hash, ShouldEqual, "hash2")
-			})
-		})
-	})
-
-	withClosedConn(t, "When get latest transaction block hash", func(s Storage) error {
-		_, err := s.GetLatestTransactionBlockHash()
-		return err
 	})
 }
