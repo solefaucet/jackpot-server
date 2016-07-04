815c1e1bd2b1b9826770e23fd9330681ab6810d3
on99 <on99@users.noreply.github.com>
2016-07-04 22:57:26 +0800

    feat(main): save block and transactions
    refs #14

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 
 	"github.com/Sirupsen/logrus"
 	"github.com/robfig/cron"
-	"github.com/solefaucet/jackpot-server/jerrors"
 	"github.com/solefaucet/jackpot-server/models"
 	s "github.com/solefaucet/jackpot-server/services/storage"
 	"github.com/solefaucet/jackpot-server/services/storage/mysql"
@@ -67,6 +66,9 @@ func initService() {
 
 	// init cronjob
 	initCronjob()
+
+	// MOST IMPORTANT FUNCTION HERE!!!
+	initWork()
 }
 
 func main() {
@@ -88,97 +90,7 @@ func main() {
 
 func initCronjob() {
 	c := cron.New()
-	utils.Must(nil, c.AddFunc("@every 1m", safeFuncWrapper(addNewBlock)))        // add new block every 1 minute
-	utils.Must(nil, c.AddFunc("@every 1m", safeFuncWrapper(addNewTransactions))) // add new transactions every 1 minute
 	c.Start()
-}
-
-func addNewBlock() {
-	entry := logrus.WithField("event", models.LogEventAddNewBlock)
-
-	// get latest block from db
-	block, err := storage.GetLatestBlock()
-	if err != nil && err != jerrors.ErrNotFound {
-		entry.WithError(err).Error("fail to get latest block from database")
-		return
-	}
-
-	// get block from blockchain
-	bestBlock := err == jerrors.ErrNotFound
-	blockchainBlock, err := wallet.GetBlock(bestBlock, block.Height+1)
-	switch {
-	case err == jerrors.ErrNoNewBlock:
-		entry.Info("no new block ahead")
-		return
-	case err != nil:
-		entry.WithError(err).Error("fail to get block from blockchain")
-		return
-	}
-
-	// save block to storage
-	block = models.Block{
-		Hash:           blockchainBlock.Hash,
-		Height:         blockchainBlock.Height,
-		BlockCreatedAt: blockchainBlock.BlockCreatedAt.UTC(),
-	}
-	if err := storage.SaveBlock(block); err != nil {
-		entry.WithError(err).Error("fail to save block to database")
-		return
-	}
-
-	entry.Info("add new block successfully")
-
-	// keep adding until no blocks is ahead
-	addNewBlock()
-}
-
-func addNewTransactions() {
-	entry := logrus.WithField("event", models.LogEventAddNewTransactions)
-
-	hash, err := storage.GetLatestTransactionBlockHash()
-	if err != nil {
-		entry.WithError(err).Error("fail to get latest transaction block hash from db")
-		return
-	}
-
-	transactions, err := wallet.GetReceivedSince(hash, config.Wallet.MinConfirms)
-	if err != nil {
-		entry.WithFields(logrus.Fields{
-			"hash":  hash,
-			"error": err,
-		}).Error("fail to get received since block")
-		return
-	}
-
-	// interrupt here
-	if len(transactions) == 0 {
-		entry.Info("no new transactions")
-		return
-	}
-
-	if err := storage.SaveTransactions(walletTxsToModelTxs(transactions)); err != nil {
-		entry.WithError(err).Error("fail to save transactions")
-		return
-	}
-
-	entry.Info("add new transactions successfully")
-
-	// keep adding until no blocks is ahead
-	addNewTransactions()
-}
-
-func walletTxsToModelTxs(txs []w.Transaction) []models.Transaction {
-	transactions := make([]models.Transaction, len(txs))
-	for i, v := range txs {
-		transactions[i] = models.Transaction{
-			Address:        v.Address,
-			Amount:         v.Amount,
-			TransactionID:  v.TransactionID,
-			Hash:           v.Hash,
-			BlockCreatedAt: v.BlockCreatedAt,
-		}
-	}
-	return transactions
 }
 
 func catch(then func()) {
