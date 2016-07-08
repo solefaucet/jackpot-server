05ab828a0c2655e4e41964c89004b64aed87c7f9
on99 <on99@users.noreply.github.com>
2016-07-08 17:32:51 +0800

    feat(mysql): implement storage interface with mysql

--- models/game.go
+++ models/game.go
@@ -2,6 +2,13 @@ package models
 
 import "time"
 
+// game status
+const (
+	GameStatusPending    = "pending"
+	GameStatusProcessing = "processing"
+	GameStatusEnded      = "ended"
+)
+
 // Game model
 type Game struct {
 	ID            int64     `db:"id"`
@@ -13,6 +20,7 @@ type Game struct {
 	Fee           float64   `db:"fee"`
 	TransactionID string    `db:"tx_id"`
 	GameOf        time.Time `db:"game_of"`
+	Status        string    `db:"status"`
 	CreatedAt     time.Time `db:"created_at"`
 	UpdatedAt     time.Time `db:"updated_at"`
 }
--- models/transaction.go
+++ models/transaction.go
@@ -8,7 +8,9 @@ type Transaction struct {
 	Address        string    `db:"address"`
 	Amount         float64   `db:"amount"`
 	TransactionID  string    `db:"tx_id"`
+	Confirmations  int64     `db:"confirmations"`
 	Hash           string    `db:"hash"`
+	GameOf         time.Time `db:"game_of"`
 	BlockCreatedAt time.Time `db:"block_created_at"`
 	CreatedAt      time.Time `db:"created_at"`
 }
