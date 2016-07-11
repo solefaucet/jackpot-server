946c1dab094cc53cc0bebe24a63d569d11c745c9
on99 <on99@users.noreply.github.com>
2016-07-12 00:18:37 +0800

    test(handler): fix broken test

--- handlers/v1/dependency_test.go
+++ handlers/v1/dependency_test.go
@@ -6,14 +6,14 @@ import (
 	"github.com/solefaucet/jackpot-server/models"
 )
 
-func mockDependencyGetGamesWithin(games []models.Game, err error) dependencyGetGamesWithin {
-	return func(_, _ time.Time) ([]models.Game, error) {
+func mockDependencyGetGames(games []models.Game, err error) dependencyGetGames {
+	return func(_, _ int64) ([]models.Game, error) {
 		return games, err
 	}
 }
 
-func mockDependencyGetTransactionsWithin(transactions []models.Transaction, err error) dependencyGetTransactionsWithin {
-	return func(_, _ time.Time) ([]models.Transaction, error) {
+func mockDependencyGetTransactionsByGameOfs(transactions []models.Transaction, err error) dependencyGetTransactionsByGameOfs {
+	return func(...time.Time) ([]models.Transaction, error) {
 		return transactions, err
 	}
 }
